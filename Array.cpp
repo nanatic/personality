5243955ce35701d5c3f77f8fba476d6d68e776c6
nanatic <96443341+nanatic@users.noreply.github.com>
2021-12-21 07:20:33 +0300

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1,5 +1,5 @@
 #include "Array.h"
-
+#include "String.cpp"
 template <class P>
 Array<P>::Array(const Array<P>& other) : size_(other.size_), array_(size_ == 0 ? nullptr : new P[size_]) {
 	for (int i = 0; i < size_; i++) {
@@ -45,4 +45,54 @@ P& Array<P>::operator[](std::size_t i)
         throw std::out_of_range("Array: Wrong index");
     }
     return array_[i];
+}
+enum Keys {
+    SURNAME,
+    NAME,
+    YEAR
+};
+template <class P>
+void Array<P>::sortArray(Keys key) {
+    for (int border = 1; border < this.length_; border++)
+    {
+        Array* step = this.array_[border];
+        int into = border - 1;
+        bool greaterThanStep;
+        switch (key)
+        {
+        case Keys::SURNAME:
+            greaterThanStep = ((this.array_[into]->getSurname()).compare(step->getSurname()) > 0);
+            break;
+        case Keys::NAME:
+            greaterThanStep = ((this.array_[into]->getName()).compare(step->getName()) > 0);
+            break;
+        case Keys::YEAR:
+            greaterThanStep = ((this.array_[into]->getYear()).compare(step->getYear()) > 0);
+            break;
+        default:
+            throw std::invalid_argument("Mishanding");
+        }
+
+        while (into >= 0 && greaterThanStep)
+        {
+            this.array_[into + 1] = this.array_[into];
+            into--;
+            if (into >= 0)
+            {
+                switch (key)
+                {
+                case Keys::SURNAME:
+                    greaterThanStep = ((this.array_[into]->getSurname()).compare(step->getSurname()) > 0);
+                    break;
+                case Keys::NAME:
+                    greaterThanStep = ((this.array_[into]->getName()).compare(step->getName()) > 0);
+                    break;
+                case Keys::YEAR:
+                    greaterThanStep = ((this.array_[into]->getYear()).compare(step->getYear()) > 0);
+                    break;
+                }
+            }
+        }
+        this.array_[++into] = step;
+    }
 }
